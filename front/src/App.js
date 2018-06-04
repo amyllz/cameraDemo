879e0618b2768e43f6f6940dfe5a93befce70bce
amyllz <xiaoxiaozhezi@163.com>
2018-06-04 21:46:31 +0800

    modify ip

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,7 @@ export default class SignIn extends Component {
       <div className="SignIn-body">
         <p style={{ textAlign: 'center', fontFamily: 'Microsoft YaHei', fontSize: '18px', paddingTop: "50px"  }}>上传图像</p>
         <Row type="flex" justify="center">
-          <img src={'http://192.168.31.35:4002/pic.jpg'} style={{ height: "500px", marginTop: "30px" }} alt="toppic" />
+          <img src={'http://119.23.210.52:4002/pic.jpg'} style={{ height: "500px", marginTop: "30px" }} alt="toppic" />
         </Row>
         <p style={{ textAlign: 'center', fontFamily: 'Microsoft YaHei', fontSize: '12px', marginTop: "50px"  }}>@杭州钛比科技有限公司</p>
       </div>
